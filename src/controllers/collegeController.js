e0072383feab1492f939d738e284858ba6882ce6
Souvik Jha <jhasouvik4@gmail.com>
2022-07-01 13:55:53 +0530

    submission

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose")
 const collegeModel = require("../models/collegeModel")
 const internModel = require("../models/internModel")
-const link = require('valid-url')
 
 const isValid = function (value) {
     if (typeof value === "undefined" || value === null) return false;
@@ -32,7 +31,8 @@ const createCollege = async function (req, res) {
 
         if (!logoLink) return res.status(400).send({ status: false, msg: "Logo Link is required" })
         if(typeof logoLink!== "string") return res.status(400).send({ status: false, message: "logo Link is invalid" })
-        if (!(link.isWebUri(logoLink))) return res.status(400).send({ status: false, message: "Logo Link is invalid" })
+        if(!logoLink.trim().match(/^(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%.\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%\+.~#?&//=]*)$/)) return res.status(400).send({status:false,message:"provide valid logo link"})
+
 
         let saveData = await collegeModel.create(requestBody)
         return res.status(201).send({ status: true, data: saveData })
@@ -45,14 +45,12 @@ const createCollege = async function (req, res) {
 const getCollegeDetails = async function (req, res) {
     try {
         let query = req.query
-        // console.log(query)
         if (!(isvalidRequest(query))) return res.status(400).send({ status: false, msg: "provide request parameter " })
         if(!query.collegeName) return res.status(400).send({ status: false, msg: "provide College Name " })
 
         if (!query.collegeName.trim().length) return res.status(400).send({ status: false, message: "provide name of college" })
 
         let getDetails = await collegeModel.findOne({name:query.collegeName}).select({ name: 1, fullName: 1, logoLink: 1, _id: 1 })
-        // console.log(getDetails)
         if (!getDetails) return res.status(404).send({ status: false, message: "no such college found" })
 
         let internDetails = await internModel.find({ collegeId: getDetails._id }).select({ name: 1, email: 1, mobile: 1 })
